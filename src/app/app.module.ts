a414011f10a50620c8f85ddc7642b044d181a349
JakeClutts <jakeclutts@gmail.com>
2021-10-04 20:17:15 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { bootstrap } from 'bootstrap'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeLibraryComponent } from './recipe-library/recipe-library.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeLibraryComponent,
+    AddRecipeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipe-library/recipe-library.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-library',
+  templateUrl: './recipe-library.component.html',
+  styleUrls: ['./recipe-library.component.css']
+})
+export class RecipeLibraryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
